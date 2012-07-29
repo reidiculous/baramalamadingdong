5c5aa3208b42c70d4abc9a998aa227f0caa631d2
Reid Lynch <reid.lynch@gmail.com>
2012-07-29 09:52:44 -0500

    app setup

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -61,13 +61,24 @@ grails.gorm.failOnError=true
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
+dataSource {
+    pooled = false
+    driverClassName = "org.h2.Driver"
+    username = "sa"
+    password = ""
+}
+
 environments {
     development {
         dataSource {
             dbCreate = "create-drop"
-            url = "jdbc:h2:mem:devDb"
+            url = "jdbc:h2:devDb"
         }
         grails.logging.jul.usebridge = true
+        log4j = {
+          info "grails.app"
+        }
+
     }
     test {
         dataSource {
